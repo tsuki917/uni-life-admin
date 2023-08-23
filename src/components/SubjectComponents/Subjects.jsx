6685fd5e65b4b41faf4759ac2e4cbc3b0fcabcd6
Cacao1522 <kaorukato2003@gmail.com>
2023-08-24 01:31:02 +0900

    データの入力（お試し）

--- src/components/SubjectComponents/Subjects.jsx
+++ src/components/SubjectComponents/Subjects.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { auth, db } from "../../libs/fire";
+import { doc, setDoc } from "firebase/firestore";
 export default function Subjects() {
   /*
     type subjectData = {
@@ -8,9 +10,72 @@ export default function Subjects() {
     */
   const subjectsData = useState([]);
 
+  const [f_Xday, setF_Xday] = useState("");
+  const [f_rate, setF_rate] = useState("0");
+  const [f_score, setF_score] = useState("0");
+  const onAddEvent = async () => {
+    const event = {
+      finalExam: {
+        Xday: f_Xday,
+        rate: Number(f_rate),
+        score: Number(f_score),
+      },
+      // middleExam:
+      // reports:
+      // smallExam:
+    };
+    await setDoc(doc(db, auth.currentUser.email, "フーリエ変換"), event);
+  };
   return (
     <div>
       <h1>subject</h1>
+      <div>中間試験</div>
+
+      <label>
+        試験日
+        <input
+          //className=
+          type="text"
+          value={f_Xday}
+          name="inputTitle"
+          onChange={(e) => {
+            setF_Xday(e.target.value);
+          }}
+        />
+      </label>
+      <label>
+        割合
+        <input
+          //className=
+          type="number"
+          value={f_rate}
+          name="inputTitle"
+          min="0"
+          max="100"
+          onChange={(e) => {
+            setF_rate(e.target.value);
+          }}
+        />
+      </label>
+      <label>
+        点数
+        <input
+          //className=
+          type="number"
+          value={f_score}
+          name="inputTitle"
+          min="0"
+          onChange={(e) => {
+            setF_score(e.target.value);
+          }}
+        />
+      </label>
+      <input
+        //className=
+        type="button"
+        value="追加"
+        onClick={onAddEvent}
+      />
     </div>
   );
 }
