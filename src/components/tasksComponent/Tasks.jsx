2f4b1bd0bc8a0de2c78188c073ec12da58c41535
tsuki917 <einstrom917@gmail.com>
2023-08-31 23:43:56 +0900

    add get only recently events

--- src/components/tasksComponent/Tasks.jsx
+++ src/components/tasksComponent/Tasks.jsx
@@ -4,36 +4,34 @@ import { db, auth } from "../../libs/fire";
 import { collection, getDocs } from "firebase/firestore";
 import Task from "./Task";
 export default function Tasks() {
-  const [reportsState, setReportsState] = useState([]);
+  const [recentsData, setRecentData] = useState([]);
 
   useEffect(() => {
-    const data = getAllReportData();
+    const data = getRecentEventData();
     data.then((ele) => {
       console.log(ele);
-      setReportsState(ele);
+      setRecentData(ele);
     });
   }, []);
 
   return (
     <Box sx={{ mr: 6, ml: 6 }}>
-      <h3>直近の課題</h3>
+      <h1>直近の課題</h1>
       <List>
-        {reportsState.map((reportData, key) => {
+        {recentsData.map((recentData, key) => {
           return (
             <Box key={key}>
-              <Task reportsData={reportData} />
+              <Task eventsData={recentData} />
             </Box>
           );
         })}
       </List>
-
-      <Button onClick={getAllReportData}>report</Button>
     </Box>
   );
 }
 
-async function getAllReportData() {
-  const reportsData = [];
+async function getRecentEventData() {
+  const eventsData = [];
   const userCollenction = collection(db, auth.currentUser.email);
   const q = await getDocs(userCollenction);
 
@@ -42,42 +40,68 @@ async function getAllReportData() {
     subjectData.reports.reportArray.forEach((reportData) => {
       const pushData = {
         subjectName: subjectData.name,
-        reportData: {},
+        reportData: reportData,
+        distDay: getDaysRemaining(reportData.deadlineDay),
       };
-      pushData.reportData = reportData;
-      // const test = new Date();
-      // console.log(test);
-      // console.log(pushData.reportData.deadlineDay);
-      // const dist =
-      //   pushData.reportData.deadlineDay.nanoseconds - Date.now() / 1000;
-      // console.log(pushData.subjectName);
-      // console.log(pushData.reportData.title);
-      // console.log("今日" + Date.now());
-      // console.log("nano" + pushData.reportData.deadlineDay.nanoseconds);
-      // console.log(Math.floor(dist / (60 * 60 * 24 * 1000)));
-      // 今日の日付を取得
-      const today = new Date();
 
-      // pushData.reportData.deadlineDayから秒とナノ秒を取得
-      const deadlineSeconds = pushData.reportData.deadlineDay.seconds;
-      const deadlineNanoseconds = pushData.reportData.deadlineDay.nanoseconds;
-
-      // タイムスタンプをミリ秒に変換
-      const deadlineTimestampMillis =
-        deadlineSeconds * 1000 + deadlineNanoseconds / 1000000;
-
-      // 締め切り日をDateオブジェクトとして生成
-      const deadlineDate = new Date(deadlineTimestampMillis);
-
-      // 今日の日付と締め切り日との日付の差を計算
-      const timeDiffMillis = deadlineDate - today;
-      const daysRemaining = Math.floor(timeDiffMillis / (1000 * 60 * 60 * 24));
-
-      console.log("Days Remaining:", daysRemaining);
-      if (daysRemaining < 8) {
-        reportsData.push(pushData);
+      if (pushData.distDay < 8 && pushData.distDay > -2) {
+        eventsData.push(pushData);
       }
     });
+    subjectData.smallExam.smallExamArray.forEach((smallExamData) => {
+      const pushData = {
+        subjectName: subjectData.name,
+        smallExamData: smallExamData,
+        distDay: getDaysRemaining(smallExamData.Xday),
+      };
+      if (pushData.distDay < 8 && pushData.distDay > -2) {
+        eventsData.push(pushData);
+      }
+    });
+
+    if (
+      getDaysRemaining(subjectData.middleExam.Xday) < 8 &&
+      getDaysRemaining(subjectData.middleExam.Xday) > -2
+    ) {
+      eventsData.push({
+        subjectName: subjectData.name,
+        middleExamData: subjectData.middleExam,
+        distDay: getDaysRemaining(subjectData.middleExam.Xday),
+      });
+    }
+    if (
+      getDaysRemaining(subjectData.finalExam.Xday) < 8 &&
+      getDaysRemaining(subjectData.finalExam.Xday) > -2
+    ) {
+      eventsData.push({
+        subjectName: subjectData.name,
+        finalExamData: subjectData.finalExam,
+        distDay: getDaysRemaining(subjectData.finalExam.Xday),
+      });
+    }
   });
-  return reportsData;
+  eventsData.sort((a, b) => {
+    return a.distDay - b.distDay;
+  });
+  return eventsData;
+}
+
+function getDaysRemaining(dayTime) {
+  const today = new Date();
+
+  // pushData.reportData.deadlineDayから秒とナノ秒を取得
+  const deadlineSeconds = dayTime.seconds;
+  const deadlineNanoseconds = dayTime.nanoseconds;
+
+  // タイムスタンプをミリ秒に変換
+  const deadlineTimestampMillis =
+    deadlineSeconds * 1000 + deadlineNanoseconds / 1000000;
+
+  // 締め切り日をDateオブジェクトとして生成
+  const deadlineDate = new Date(deadlineTimestampMillis);
+
+  // 今日の日付と締め切り日との日付の差を計算
+  const timeDiffMillis = deadlineDate - today;
+  const daysRemaining = Math.floor(timeDiffMillis / (1000 * 60 * 60 * 24));
+  return daysRemaining;
 }
