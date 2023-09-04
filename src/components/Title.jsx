5ddba2a1b5c9b1dfe86e27fd65144bcd48b77bf1
tsuki917 <einstrom917@gmail.com>
2023-09-04 21:55:15 +0900

    merge master

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 import {
   AppBar,
@@ -24,6 +26,8 @@ const rightLink = {
 };
 
 export default function Title({ login }) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const navigation = useNavigate();
   const [toggleView, setToggleView] = useState("");
 
@@ -39,139 +43,252 @@ export default function Title({ login }) {
 
   return (
     <div>
-      <AppBar position="fixed" sx={{ ml: 1, mr: 1 }}>
-        <Toolbar sx={{ justifyContent: "space-between" }}>
-          <Button
-            variant="h6"
-            underline="none"
-            color="inherit"
-            sx={{ fontSize: 24 }}
-            LinkComponent={Link}
-            to="/"
-            // onClick={(e) => {
-            //   pageChange(e);
-            // }}
-          >
-            GPAJourney
-          </Button>
-          {auth.currentUser !== null ? (
-            <ToggleButtonGroup
-              exclusive
-              color="primary"
-              value={toggleView}
-              onChange={handleToggleChange}
+      {isMobile ? (
+        <Box>
+          <AppBar position="fixed" sx={{ ml: 1, mr: 1, height: 200 }}>
+            <Toolbar
               sx={{
-                "& .MuiToggleButton-root.Mui-selected": {
-                  backgroundColor: "#d9d9db", // 選択されているアイテムの背景色を指定
-                  color: "black", // 選択されているアイテムのテキスト色を指定
-                },
-                "& .MuiToggleButton-root.Mui-selected:hover": {
-                  backgroundColor: "#d9d9db", // ホバー時の背景色を選択されたアイテムと同じに設定
-                  color: "black", // ホバー時のテキスト色も選択されたアイテムと同じに設定
-                },
+                justifyContent: "space-between",
+                display: "flex",
+                height: 100,
               }}
             >
-              <ToggleButton
-                value="home"
-                aria-label="home"
-                LinkComponent={Link}
-                to="/"
-                sx={{
-                  bgcolor: "background.paper",
-                  "&:hover": {
-                    bgcolor: "#d9d9db", // ホバー時の背景色も指定（必要に応じて）
-                  },
-                  ml: 4,
-                  fontSize: 12,
-                }}
-              >
-                <HomeIcon />
-                Home
-              </ToggleButton>
-              <ToggleButton
-                value="subject"
-                aria-label="subject"
-                LinkComponent={Link}
-                to="/subjects"
-                sx={{
-                  bgcolor: "background.paper",
-                  "&:hover": {
-                    bgcolor: "#d9d9db", // ホバー時の背景色も指定（必要に応じて）
-                  },
-                  ml: 4,
-                  fontSize: 12,
-                }}
-              >
-                <LocalLibrary />
-                subject
-              </ToggleButton>
-              <ToggleButton
-                value="tasks"
-                aria-label="tasks"
-                LinkComponent={Link}
-                to="/tasks"
-                sx={{
-                  bgcolor: "background.paper",
-                  "&:hover": {
-                    bgcolor: "#d9d9db", // ホバー時の背景色も指定（必要に応じて）
-                  },
-                  ml: 4,
-                  fontSize: 12,
-                }}
-              >
-                <Assignment />
-                　task　
-              </ToggleButton>
-            </ToggleButtonGroup>
-          ) : (
-            ""
-          )}
-
-          <Box
-            sx={{
-              flex: 1,
-              display: "flex",
-              justifyContent: "flex-end",
-              alignItems: "flex-start",
-            }}
-          >
-            {auth.currentUser ? (
               <Box
                 sx={{
-                  pt: 1,
-                  pb: 1,
+                  justifyContent: "flex-start",
+                  flexDirection: "column",
+                  display: "flex",
+                }}
+              >
+                <Button
+                  variant="h6"
+                  underline="none"
+                  color="inherit"
+                  sx={{ fontSize: 10 }}
+                  LinkComponent={Link}
+                  to=""
+                  // onClick={(e) => {
+                  //   pageChange(e);
+                  // }}
+                >
+                  GPAJourney
+                </Button>
+
+                {auth.currentUser !== null ? (
+                  <ToggleButtonGroup
+                    exclusive
+                    color="primary"
+                    value={toggleView}
+                    onChange={handleToggleChange}
+                    sx={{
+                      "& .MuiToggleButton-root.Mui-selected": {
+                        backgroundColor: "#d9d9db", // 選択されているアイテムの背景色を指定
+                        color: "black", // 選択されているアイテムのテキスト色を指定
+                      },
+                    }}
+                  >
+                    <ToggleButton
+                      value="subject"
+                      aria-label="subject"
+                      LinkComponent={Link}
+                      to="/subjects"
+                      sx={{
+                        bgcolor: "background.paper",
+                        "&:hover": {
+                          bgcolor: "#d9d9db", // ホバー時の背景色も指定（必要に応じて）
+                        },
+                        ml: 4,
+                        fontSize: 12,
+                      }}
+                    >
+                      <LocalLibrary />
+                      subject
+                    </ToggleButton>
+                    <ToggleButton
+                      value="tasks"
+                      aria-label="tasks"
+                      LinkComponent={Link}
+                      to="/tasks"
+                      sx={{
+                        bgcolor: "background.paper",
+                        "&:hover": {
+                          bgcolor: "#d9d9db", // ホバー時の背景色も指定（必要に応じて）
+                        },
+                        ml: 4,
+                        fontSize: 12,
+                      }}
+                    >
+                      <Assignment />
+                      　task　
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                ) : (
+                  ""
+                )}
+              </Box>
+
+              <Box
+                sx={{
                   flex: 1,
                   display: "flex",
                   justifyContent: "flex-end",
-                  alignItems: "center",
-                  borderRadius: "3",
-                  width: 1,
+                  alignItems: "flex-start",
                 }}
               >
-                {auth.currentUser.email}
-                <Button onClick={login}>
-                  <img
-                    src={auth.currentUser.photoURL}
-                    alt="アイコン"
-                    style={{ marginLeft: 10, borderRadius: 30 }}
-                    width="30"
-                    height="30"
-                  ></img>
-                </Button>
+                {auth.currentUser ? (
+                  <Box
+                    sx={{
+                      pt: 1,
+                      pb: 1,
+                      flex: 1,
+                      display: "flex",
+                      justifyContent: "flex-end",
+                      alignItems: "center",
+                      borderRadius: "3",
+                      width: 1,
+                    }}
+                  >
+                    {auth.currentUser.email}
+                    <Button onClick={login}>
+                      <img
+                        src={auth.currentUser.photoURL}
+                        alt="アイコン"
+                        style={{ marginLeft: 10, borderRadius: 30 }}
+                        width="30"
+                        height="30"
+                      ></img>
+                    </Button>
+                  </Box>
+                ) : (
+                  <Button
+                    onClick={loginDeal}
+                    variant="outlined"
+                    sx={{ color: "white", borderColor: "white" }}
+                  >
+                    ログイン
+                  </Button>
+                )}
               </Box>
-            ) : (
+            </Toolbar>
+          </AppBar>
+          <Toolbar sx={{ height: 200 }} />
+        </Box>
+      ) : (
+        <Box>
+          <AppBar position="fixed" sx={{ ml: 1, mr: 1 }}>
+            <Toolbar sx={{ justifyContent: "space-between" }}>
               <Button
-                onClick={loginDeal}
-                variant="outlined"
-                sx={{ color: "white", borderColor: "white" }}
+                variant="h6"
+                underline="none"
+                color="inherit"
+                sx={{ fontSize: 24 }}
+                LinkComponent={Link}
+                to=""
+                // onClick={(e) => {
+                //   pageChange(e);
+                // }}
               >
-                ログイン
+                GPAJourney
               </Button>
-            )}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
+              {auth.currentUser !== null ? (
+                <ToggleButtonGroup
+                  exclusive
+                  color="primary"
+                  value={toggleView}
+                  onChange={handleToggleChange}
+                  sx={{
+                    "& .MuiToggleButton-root.Mui-selected": {
+                      backgroundColor: "#d9d9db", // 選択されているアイテムの背景色を指定
+                      color: "black", // 選択されているアイテムのテキスト色を指定
+                    },
+                  }}
+                >
+                  <ToggleButton
+                    value="subject"
+                    aria-label="subject"
+                    LinkComponent={Link}
+                    to="/subjects"
+                    sx={{
+                      bgcolor: "background.paper",
+                      "&:hover": {
+                        bgcolor: "#d9d9db", // ホバー時の背景色も指定（必要に応じて）
+                      },
+                      ml: 4,
+                      fontSize: 12,
+                    }}
+                  >
+                    <LocalLibrary />
+                    subject
+                  </ToggleButton>
+                  <ToggleButton
+                    value="tasks"
+                    aria-label="tasks"
+                    LinkComponent={Link}
+                    to="/tasks"
+                    sx={{
+                      bgcolor: "background.paper",
+                      "&:hover": {
+                        bgcolor: "#d9d9db", // ホバー時の背景色も指定（必要に応じて）
+                      },
+                      ml: 4,
+                      fontSize: 12,
+                    }}
+                  >
+                    <Assignment />
+                    　task　
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              ) : (
+                ""
+              )}
+
+              <Box
+                sx={{
+                  flex: 1,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  alignItems: "flex-start",
+                }}
+              >
+                {auth.currentUser ? (
+                  <Box
+                    sx={{
+                      pt: 1,
+                      pb: 1,
+                      flex: 1,
+                      display: "flex",
+                      justifyContent: "flex-end",
+                      alignItems: "center",
+                      borderRadius: "3",
+                      width: 1,
+                    }}
+                  >
+                    {auth.currentUser.email}
+                    <Button onClick={login}>
+                      <img
+                        src={auth.currentUser.photoURL}
+                        alt="アイコン"
+                        style={{ marginLeft: 10, borderRadius: 30 }}
+                        width="30"
+                        height="30"
+                      ></img>
+                    </Button>
+                  </Box>
+                ) : (
+                  <Button
+                    onClick={loginDeal}
+                    variant="outlined"
+                    sx={{ color: "white", borderColor: "white" }}
+                  >
+                    ログイン
+                  </Button>
+                )}
+              </Box>
+            </Toolbar>
+          </AppBar>
+          <Toolbar />
+        </Box>
+      )}
     </div>
   );
 }
